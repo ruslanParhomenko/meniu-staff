cd97af9b8b4be304186454d34190f03e67de2e2b
ruslan-parhomenko <parhomenkogm@gmail.com>
2025-08-05 13:38:23 +0300

    add sign without google

--- src/components/ScheduleView.tsx
+++ src/components/ScheduleView.tsx
@@ -6,8 +6,6 @@ const SHEET_ID = process.env.SHEET_ID;
 const API_KEY = process.env.API_KEY;
 const RANGE = "Waiters!D55:AM75";
 
-console.log("SHEET_ID", SHEET_ID);
-
 export default function GoogleSheetTable() {
   const [data, setData] = useState<string[][]>([]);
   const [error, setError] = useState<string | null>(null);
@@ -24,17 +22,14 @@ export default function GoogleSheetTable() {
         const json = await response.json();
         const values: string[][] = json.values || [];
 
-        // Узнаём количество строк и столбцов в диапазоне
-        const totalRows = 21; // 75 - 55 + 1
-        const totalCols = 36; // AM - D + 1 (D=4, AM=39) => 39 - 4 + 1 = 36
+        const totalRows = 21;
+        const totalCols = 36;
 
-        // Создаём массив с пустыми ячейками
         const filledData: string[][] = [];
 
         for (let r = 0; r < totalRows; r++) {
           filledData[r] = [];
           for (let c = 0; c < totalCols; c++) {
-            // Если есть значение, возьмём его, иначе пустая строка
             filledData[r][c] = values[r]?.[c] ?? "";
           }
         }
@@ -53,9 +48,6 @@ export default function GoogleSheetTable() {
     <table className="border-collapse w-full">
       <tbody>
         {data.map((row, i) => {
-          // Пропустить строку, если ячейка H (index 7) пустая
-          //   if (!row[5] || String(row[5]).trim() === "") return null;
-
           return (
             <tr key={i}>
               {row.map((cell, j) => {
@@ -67,7 +59,6 @@ export default function GoogleSheetTable() {
                 const isFirstRow = i === 0 || i === 1 || i === data.length - 1;
                 const isFirstThreeColumns = j < 4;
 
-                // Логика бордеров
                 let borderClass = "";
                 if (isFirstRow || isFirstThreeColumns) {
                   borderClass = `text-center text-sm ${
