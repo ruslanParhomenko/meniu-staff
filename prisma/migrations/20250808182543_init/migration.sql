e21c63e1d01919c0fa7d1c05215345bbfd90e875
parhomenco <parhomenkogm@gmail.com>
2025-08-11 21:32:34 +0300

    switch to postgres

--- prisma/migrations/20250808182543_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Employee" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "position" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
