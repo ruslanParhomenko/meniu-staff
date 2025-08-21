868ff3be7814c62f3d9b8b4bbade2127bc5f18cd
NBM <cng.it.department@gmail.com>
2025-08-21 11:26:55 +0300

    fix data sheldue

--- src/components/wrapper/OrderCardWrapper.tsx
+++ src/components/wrapper/OrderCardWrapper.tsx
@@ -4,6 +4,7 @@ import { Label } from "../ui/label";
 import { QUANTITY_SELECT } from "@/features/order-list/constants";
 import { Separator } from "../ui/separator";
 import { useAbility } from "@/providers/AbilityProvider";
+import { useFormContext, useWatch } from "react-hook-form";
 
 export function OrderCardWrapper({
   data,
@@ -13,20 +14,33 @@ export function OrderCardWrapper({
   name: string;
 }) {
   const { isObserver } = useAbility();
+  const { setValue, control } = useFormContext();
   return (
     <div>
       <div className="flex flex-col w-full justify-center items-center py-2">
         <Label className="py-2  font-bold text-blue-600">{name}</Label>
       </div>
       {data.map((item, index) => {
+        const value = useWatch({ control, name: item });
         return (
           <div key={index}>
-            <SelectInput
-              fieldName={item}
-              fieldLabel={item}
-              data={QUANTITY_SELECT}
-              disabled={isObserver}
-            />
+            <div className="grid-cols-[95%_5%] grid">
+              <SelectInput
+                fieldName={item}
+                fieldLabel={item}
+                data={QUANTITY_SELECT}
+                disabled={isObserver}
+              />
+              {value && (
+                <button
+                  type="button"
+                  className=" text-red-600 front-bold"
+                  onClick={() => setValue(item, "")}
+                >
+                  -
+                </button>
+              )}
+            </div>
             {data.length - 1 !== index && <Separator className="my-1" />}
           </div>
         );
--- src/features/order-list/constants.ts
+++ src/features/order-list/constants.ts
@@ -440,9 +440,11 @@ export const FRUITBOX_C = [
 ];
 
 export const QUANTITY_SELECT = [
-  { label: "0", value: "0" },
+  { label: "0.5", value: "0.5" },
   { label: "1", value: "1" },
+  { label: "1.5", value: "1.5" },
   { label: "2", value: "2" },
+  { label: "2.5", value: "2.5" },
   { label: "3", value: "3" },
   { label: "4", value: "4" },
   { label: "5", value: "5" },
