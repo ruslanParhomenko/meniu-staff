2e79589ac3b05e6cd199d7a5a8b58a2f5f74e6a4
NBM <cng.it.department@gmail.com>
2025-08-21 10:33:23 +0300

    fix data sheldue

--- src/features/schedule/ScheduleForm.tsx
+++ src/features/schedule/ScheduleForm.tsx
@@ -11,12 +11,15 @@ import {
 
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 import { useSheetData } from "@/hooks/use-schedule-data-google";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
 export const ScheduleForm = () => {
-  const [dataRange, setDataRange] = useState<string>("Waiters!D58:AM80");
-  const { data, loading } = useSheetData({ range: dataRange as string });
+  const [dataRange, setDataRange] = useState<string>("Waiters!D57:AM78");
+  const { data } = useSheetData({ range: dataRange as string });
+  const todayDay = new Date().getDate();
+
+  const [selectedColumn, setSelectedColumn] = useState<number | null>(null);
 
   const form = useForm();
 
@@ -31,6 +34,18 @@ export const ScheduleForm = () => {
     const selected = selectData.find((item) => item.value === value);
     if (selected) setDataRange(selected.value);
   };
+
+  useEffect(() => {
+    if (data && data.length > 0) {
+      const firstRow = data[0];
+      const index = firstRow.findIndex(
+        (cell) => String(cell) === String(todayDay)
+      );
+      if (index !== -1) {
+        setSelectedColumn(index);
+      }
+    }
+  }, [data, todayDay]);
 
   return (
     <div className="w-full overflow-x-auto">
@@ -53,55 +68,66 @@ export const ScheduleForm = () => {
               </SelectContent>
             </Select>
           </div>
-          {data ? (
-            <Table>
-              <TableBody>
-                {data.map((row, i) => {
-                  return (
-                    <TableRow key={i}>
-                      {row.map((cell, j) => {
-                        const isText = j === 4;
+          <Table>
+            <TableBody>
+              {data.map((row, i) => {
+                const hasValueInSelected =
+                  selectedColumn !== null && row[selectedColumn];
 
-                        const isFirstRow =
-                          i === 0 || i === 1 || i === data.length - 1;
-                        const isFirstThreeColumns = j < 5;
+                return (
+                  <TableRow key={i}>
+                    {row.map((cell, j) => {
+                      const isText = j === 4;
+                      const isFirstRow =
+                        i === 0 || i === 1 || i === data.length - 1;
+                      const isFirstThreeColumns = j < 5;
 
-                        let borderClass = "";
-                        if (isFirstRow || isFirstThreeColumns) {
-                          borderClass = ` ${
-                            j === 4
-                              ? " text-black text-left"
-                              : "text-sm text-center"
-                          }  ${j === 3 ? "text-black pl-4" : "text-blue-600"}`;
-                        } else if (!isFirstThreeColumns && !isFirstRow) {
-                          borderClass = "";
-                        }
-
-                        return (
-                          <TableCell
-                            key={j}
-                            className={`
-                        ${borderClass} h-9 w-9  
-                        ${isText ? "text-blue-600" : ""}
-                        ${
+                      let borderClass = "";
+                      if (isFirstRow || isFirstThreeColumns) {
+                        borderClass = ` ${
                           j === 4
-                            ? "min-w-[40px] sticky left-0 z-10 text-left"
-                            : "text-center"
-                        }
-                      `}
-                          >
-                            {cell}
-                          </TableCell>
-                        );
-                      })}
-                    </TableRow>
-                  );
-                })}
-              </TableBody>
-            </Table>
-          ) : (
-            <div>Schedule not found...</div>
-          )}
+                            ? "text-black text-left sticky left-0 z-10"
+                            : "text-sm text-center"
+                        }  ${j === 3 ? "text-black pl-4" : "text-blue-600"}`;
+                      }
+
+                      const isHighlighted = selectedColumn === j;
+
+                      const shouldEmphasize =
+                        isText && hasValueInSelected && i !== 0;
+
+                      return (
+                        <TableCell
+                          key={j}
+                          onClick={() => {
+                            if (i === 0) {
+                              setSelectedColumn((prev) =>
+                                prev === j ? null : j
+                              );
+                            }
+                          }}
+                          className={`
+                      ${borderClass} h-9 w-9
+                      ${isText ? "text-blue-600" : ""}
+                      ${
+                        j === 4
+                          ? "min-w-[40px] sticky left-0 z-10 text-left"
+                          : "text-center"
+                      }
+                      ${isHighlighted ? "bg-gray-300" : ""}
+                      ${i === 0 ? "cursor-pointer" : ""}
+                      ${shouldEmphasize ? "font-bold text-red-600" : ""}
+                    `}
+                        >
+                          {cell}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
         </form>
       </Form>
     </div>
