befc2fcace5d9592855e72b2cc4cd800c91f3939
NBM <cng.it.department@gmail.com>
2025-08-28 15:30:56 +0300

    fix api

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -3,9 +3,9 @@ import { NextResponse } from "next/server";
 
 export async function DELETE(
   req: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const { id } = params;
+  const { id } = await params;
 
   await prisma.user.delete({
     where: { id: Number(id) },
