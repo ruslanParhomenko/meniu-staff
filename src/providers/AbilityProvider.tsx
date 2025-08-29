1f0a1df4b6344bbb716931de0f03894db74fb2dd
NBM <cng.it.department@gmail.com>
2025-08-29 16:14:10 +0300

    fix data base

--- src/providers/AbilityProvider.tsx
+++ src/providers/AbilityProvider.tsx
@@ -47,7 +47,7 @@ export function AbilityProvider({ children }: { children: React.ReactNode }) {
     if (!email) return;
 
     const isAdmin =
-      email === "parhomenkogm@gmail.com" ||
+      email === "cng.nv.rstrnt.mngr@gmail.com" ||
       userData.some((u) => u.role === "ADMIN" && u.mail === email);
     const isBar =
       email === "cng.nv.rstrnt@gmail.com" ||
