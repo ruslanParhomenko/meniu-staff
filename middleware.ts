a898f8e431233cb1329a7bc78efe26aced74d2de
NBM <cng.it.department@gmail.com>
2025-09-09 16:37:31 +0300

    add mode

--- middleware.ts
+++ middleware.ts
@@ -5,10 +5,16 @@ export const config = {
   matcher: ["/((?!_next|api|favicon.ico).*)"],
 };
 
-export default async function middleware(req: NextRequest) {
-  const isMode = await get<boolean>("mainMode");
-  if (isMode) {
+export async function middleware(req: NextRequest) {
+  const mainMode = await get("mainMode");
+
+  console.log("mainMode", mainMode);
+
+  // Если mainMode === false → редиректим на /loading
+  if (mainMode === false && !req.nextUrl.pathname.startsWith("/loading")) {
     return NextResponse.redirect(new URL("/loading", req.url));
   }
+
+  // Если mainMode === true → пропускаем
   return NextResponse.next();
 }
