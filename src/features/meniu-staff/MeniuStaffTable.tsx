1aef8589acf576c163b263cab3995839a5051a61
NBM <cng.it.department@gmail.com>
2025-11-08 13:20:36 +0200

    fix package

--- src/features/meniu-staff/MeniuStaffTable.tsx
+++ src/features/meniu-staff/MeniuStaffTable.tsx
@@ -73,13 +73,13 @@ export default function MeniuStaffTable({
 
           <AccordionContent
             className={cn(
-              "overflow-hidden transition-all duration-500 ease-in-out px-1"
+              "overflow-hidden transition-all duration-600 ease-in-out px-0"
             )}
           >
             <div
               className={cn(
-                "flex flex-col gap-3 py-2 transition-all duration-1000 ease-in-out",
-                delayedOpen ? "px-6" : "px-1"
+                "flex flex-col gap-3 py-2 transition-all duration-600 ease-in-out",
+                delayedOpen ? "px-4" : ""
               )}
             >
               {dataStaff?.[nameTag]
